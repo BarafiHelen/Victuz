8a9a89830ad781bb41e56edeb29a5113851218aa
Jordy Weißerth <2065818weisserth@zuyd.nl>
2025-01-31 11:49:02 +0100

    Test API3 JW

---
+++ Victuz/APIclass.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Victuz
+{
+    public class APIclass
+    {
+        private readonly HttpClient httpClient;
+
+        public APIclass()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://official-joke-api.appspot.com/random_joke");
+
+        }
+
+        public async Task<List<Post>> GetPostsAsync(); 
+        {
+            var response = await HttpClient.GetStringAsync("/posts");
+            return JsonSerializer.Deserialize<List<Post>>(response);
+        }
+
+    }
+}
---
+++ Victuz/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Victuz.Models;
+
+namespace Victuz.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly APIclass APIclass;
+        public ObservableCollection<Post> Posts { get; private set; } = new();
+
+        public MainViewModel()
+        {
+            APIclass = new APIclass();
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            var posts = await APIclass.GetPostsAsync();
+            foreach (var post in posts)
+            { 
+                posts.Add(post);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) 
+        
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    
+    }
+}
